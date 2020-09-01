1eb026cb25e3562daea92bdcbd8d60d919c93aaf
Vlad Boldyr <boldurva@mir-omsk.ru>
2020-09-01 16:59:42 +0600

    add css for input

--- src/components/Clients.js
+++ src/components/Clients.js
@@ -10,10 +10,10 @@ export const Clients = () => {
     return (
         <div>
             <div className="row">
-                <form className="col s12">
-                    <div className="col s6" style={{marginLeft:'20px'}}>
-                        <div className="input-field col s6">
-                            <i className="material-icons col s1">search</i>
+                <form>
+                    <div className="row" style={{marginLeft:'20px'}}>
+                        <div className="input-field  suffix col s6">
+                            <i className="material-icons">search</i>
                             <input id="icon_search" type="text" className="validate"/>
                             <label form="icon_search">Enter Name</label>
                         </div>
