fe4da0826f579a01f8da7b7f8376c2ca7f07b246
Vlad Boldyr <boldurva@mir-omsk.ru>
2020-09-04 16:53:18 +0600

    add locale ru datepicker

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,8 +1,27 @@
 import React, {Component,Fragment, useState,useEffect} from 'react';
+import DatePicker from "react-datepicker/dist/react-datepicker.min";
+import  "react-datepicker/dist/react-datepicker-cssmodules.min.css"
+import  "react-datepicker/dist/react-datepicker.min.css";
 import M from "materialize-css";
+import "./Modal.css";
+import ru from "date-fns/locale/ru"
+import {registerLocale} from "react-datepicker";
 
+registerLocale("ru", ru);
 
 class Modal extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            startDate : new Date()
+        }
+        this.setStartDate = this.setStartDate.bind(this);
+    };
+
+  setStartDate(date) {
+   this.setState({startDate : date});
+  };
+
   componentDidMount() {
     const options = {
        onOpenStart: () => {
@@ -31,31 +50,34 @@ class Modal extends Component {
     return (
         <Fragment>
           <button data-target="modalAddClient" type="button" className="btn-floating btn-small waves-effect waves-light modal-trigger buttonPlus"><i className="material-icons">add</i></button>
-          <div  ref={Modal => {this.Modal = Modal;}} id="modalAddClient" className="modal">
+          <div  ref={Modal => {this.Modal = Modal;}} id="modalAddClient" className="modal customModal">
             <div className="modal-content">
-              <div class="row">
+              <div className="row">
                 <form className="col s12">
                   <div className="row">
                     <div className="input-field col s4">
                       <i className="material-icons prefix">account_circle</i>
                       <input id="icon_prefix" type="text" className="validate"/>
-                      <label for="icon_prefix">Введите Имя</label>
+                      <label htmlFor="icon_prefix">Введите Имя</label>
                     </div>
                     <div className="input-field col s4">
                       <i className="material-icons prefix">phone</i>
                       <input id="icon_telephone" type="number" className="validate"/>
-                      <label for="icon_telephone">Телефон</label>
+                      <label htmlFor="icon_telephone">Телефон</label>
                     </div>
                     <div className="input-field col s4">
-                      <i className="material-icons prefix">account_circle</i>
-                      <input id="icon_prefix" type="text" className="validate"/>
-                      <label for="icon_prefix"></label>
+                        <DatePicker
+                            dateFormat="dd/MM/yyyy"
+                            selected={this.state.startDate}
+                            onChange={date => this.setStartDate(date)}RT45Y
+                            locale='ru'
+                        />
                     </div>
                   </div>
                 </form>
               </div>
             </div>
-            <div className="modal-footer">
+            <div className="modal-footer customModalFooter">
               <a href="#!" onClick={()=> console.log("Save")} className="modal-close waves-effect waves-green btn-flat">Сохранить</a>
               <a href="#!" onClick={()=> console.log("Cancel")}className="modal-close waves-effect waves-green btn-flat">Отмена</a>
             </div>
