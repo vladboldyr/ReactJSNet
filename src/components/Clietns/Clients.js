ba0c5c941199172eb6d8549cfb5b490d02853b18
Vlad Boldyr <boldurva@mir-omsk.ru>
2020-09-11 16:31:47 +0600

    ~

--- src/components/Clietns/Clients.js
+++ src/components/Clietns/Clients.js
@@ -6,10 +6,12 @@ import data from '../../data.json';
 
 export const Clients = () => {
     const [clients,changeClients] = useState([]);
+    const [filterText,setFilterText] = useState('');
 
     useEffect(() => {
         changeClients(parserData);
     },[]);
+
 
    const addNewClient = newClient => {
        changeClients([...clients,...newClient]);
@@ -27,12 +29,21 @@ export const Clients = () => {
             <div className="row">
                 <form>
                     <div className="row" style={{marginLeft:'20px'}}>
-                        <div className="input-field suffix col s2">
-                            <i className="material-icons prefix">search</i>
-                            <input id="icon_search" type="text" className="validate"/>
-                            <label htmlFor="icon_search">Введи имя</label>
+                        <div>
+                            <div className="input-field suffix col s2">
+                                <i className="material-icons prefix">search</i>
+                                <input id="icon_search" type="text"  className="validate" value={filterText} onChange={(e) => setFilterText(e.target.value)}/>
+                                <label htmlFor="icon_search">Введи имя</label>
+                            </div>
+                            <div className="col s1">
+                                <button className="buttonClear shiftButtonClear" type="button" name="clear" onClick={()=>setFilterText('')}>
+                                    <i className="small material-icons center">clear</i>
+                                </button>
+                            </div>
                         </div>
-                        <Modal countClients={clients.length} addNewClient={addNewClient}/>
+                        <div className="col s2 offset-s7">
+                            <Modal countClients={clients.length} addNewClient={addNewClient}/>
+                        </div>
                     </div>
                 </form>
             </div>
