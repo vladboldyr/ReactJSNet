27fcd57c23616cad404a3897685e79c30954f7ad
Vlad Boldyr <boldurva@mir-omsk.ru>
2020-09-18 14:17:11 +0600

    add header

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,16 +1,36 @@
 import React from 'react';
-import {Price} from '../Price/price';
-import {Works} from '../MyWorks/works';
-import {Contacts} from '../Contacts/contacts';
+import './main.css';
 
+const imgSugar = require('../../icon/sugar.png');
 
 export const Main = () => {
     return(
-        <>
-            <div>Main</div>
-            <Price />
-            <Works />
-            <Contacts />
-        </>
+        <header className="sugar-header">
+            <div className="wrapper sugar-header__wrapper">
+                <div className="sugar-header__start">
+                    <img style = {{width:'45px',height:'45px'}} src = {imgSugar}/>
+                    <a href="#" className="brand">
+                        SugarPro
+                        Studio
+                    </a>
+                </div>
+                <div className="sugar-header__middle">
+                    <nav className="nav navCustom">
+                        <button className="nav__toggle" aria-expanded="false" type="button">
+                            Меню
+                        </button>
+                        <ul className="nav__wrapper">
+                            <li className="nav__item"><a href="#">Home</a></li>
+                            <li className="nav__item"><a href="#">Price</a></li>
+                            <li className="nav__item"><a href="#">Works</a></li>
+                            <li className="nav__item"><a href="#">Contacts</a></li>
+                        </ul>
+                    </nav>
+                </div>
+                <div className="sugar-header__end">
+                    <a className="button" href="#">Sign in</a>
+                </div>
+            </div>
+        </header>
     );
 }
