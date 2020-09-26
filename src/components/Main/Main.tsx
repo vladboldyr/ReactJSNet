689d27f347d3d759fd76ea70e2022fb830668bb1
vladboldyr <vladiclav.bolduryxa@gmail.com>
2020-09-26 17:24:18 +0600

    add icon for аутентификации

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,13 +1,15 @@
 import React, { useState } from 'react';
 import { Route, Switch, Redirect, withRouter ,NavLink } from 'react-router-dom';
-import {Clients} from '../Clietns/clients';
-import {Home} from '../Home/home';
-import {Price} from '../Price/price';
-import {Works} from '../MyWorks/works';
-import {Contacts} from '../Contacts/contacts';
+import { Clients } from '../Clietns/clients';
+import { Home } from '../Home/home';
+import { Price } from '../Price/price';
+import { Works } from '../MyWorks/works';
+import { Comments } from '../Comments/comments';
+import { Contacts } from '../Contacts/contacts';
 import './main.less';
 
-const imgSugar = require('../../icon/sugar.png');
+const imgSugar = require('../../icons/sugar.png');
+const authentication = require('../../icons/authentication.svg');
 
 export const Main = () => {
     const [activeMenu,isActiveMenu] = useState(false);
@@ -32,11 +34,14 @@ export const Main = () => {
                             <li className="sugar-header__link"><NavLink to="/" exact>О Студии</NavLink></li>
                             <li className="sugar-header__link"><NavLink to="/price">Прайс</NavLink></li>
                             <li className="sugar-header__link"><NavLink to="/works">Наши работы</NavLink></li>
+                            <li className="sugar-header__link"><NavLink to="/comments">Отзовы</NavLink></li>
                             <li className="sugar-header__link"><NavLink to="/contacts">Контакты</NavLink></li>
                          </ul>
                     </nav>
                     <div className="sugar-header__end">
-                      <a className="button" href="#">Sign in</a> 
+                      <a className="button" href="#">
+                        <img src={authentication} alt="Авторизируйтесь"></img>
+                      </a> 
                     </div>
                 </div>
             </div>
@@ -47,6 +52,7 @@ export const Main = () => {
                 <Route path={'/'} exact component={Home} />
                 <Route path={'/price'} component={Price} />
                 <Route path={'/works'} component={Works} />
+                <Route path={'/comments'} component={Comments} />
                 <Route path={'/contacts'} component={Contacts} />
                 <Redirect from='/' to='/'/>
             </Switch>
