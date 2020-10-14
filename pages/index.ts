8d533c48372b228ab1c9d68dff56c975609d5ae5
Vlad Boldyr <boldurva@mir-omsk.ru>
2020-10-14 09:13:57 +0600

    ~

--- pages/index.ts
+++ pages/index.ts
@@ -1,6 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+/* import React from 'react';
+import ReactDOM from 'react-dom'; */
 
 export default function Index(){
-  return <h1>Hello</h1>
+  return <h1> Hello </h1>
 }
