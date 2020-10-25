349e8b712fb3a319d6052486c08ed3e0e10e1385
vladboldyr <vladiclav.bolduryxa@gmail.com>
2020-10-25 20:21:06 +0600

    count cost procedure

--- src/components/Price/price.tsx
+++ src/components/Price/price.tsx
@@ -2,6 +2,11 @@ import React, { useState,useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import DataTable from 'react-data-table-component';
 
+interface Row {
+  id: string;
+  name: string;
+  cost: number;
+}
 
 const customStyles = {
   rows: {
@@ -38,38 +43,79 @@ const columns = [
 ]
 
 const price = [
-    { id: uuidv4(), name: 'Руки', cost: 'Руки' },
-    { id: uuidv4(), name: 'Ноги полностью', cost: 'Ноги полностью'},
-    { id: uuidv4(), name: 'Бедра', cost: 'Бедра'},
-    { id: uuidv4(), name: 'Голени', cost: 'Голени'},
-    { id: uuidv4(), name: 'Руки полностью', cost: 'Руки полностью'},
-    { id: uuidv4(), name: 'До локтя', cost: 'До локтя'},
-    { id: uuidv4(), name: 'От локтя', cost: 'От локтя'},
-    { id: uuidv4(), name: 'Зона на лице', cost: 'Зона на лице'},
-    { id: uuidv4(), name: 'Подмышечные впадины', cost: 'Подмышечные впадины' },
-    { id: uuidv4(), name: 'Спина', cost: 'Спина' },
-    { id: uuidv4(), name: 'Живот', cost: 'Живот'},
-    { id: uuidv4(), name: 'Дорожка на животе', cost: 'Дорожка на животе'},
-    { id: uuidv4(), name: 'Ягодицы', cost: 'Ягодицы'},
-    { id: uuidv4(), name: 'Классическое бикини', cost: 'Классическое бикини'},
-    { id: uuidv4(), name: 'Глубокое бикини', cost: 'Глубокое бикини'},
-    { id: uuidv4(), name: 'Грудь', cost: 'Грудь' } 
+    { id: uuidv4(), name: 'Руки до локтя', cost: 400 },
+    { id: uuidv4(), name: 'Ноги полностью', cost: 1000},
+    { id: uuidv4(), name: 'Бедра', cost: 500},
+    { id: uuidv4(), name: 'Голени', cost: 500},
+    { id: uuidv4(), name: 'Руки полностью', cost: 500},
+    { id: uuidv4(), name: 'Зона на лице', cost: 100},
+    { id: uuidv4(), name: 'Подмышечные впадины', cost: 200},
+    { id: uuidv4(), name: 'Спина', cost: 500},
+    { id: uuidv4(), name: 'Дорожка на животе', cost: 100},
+    { id: uuidv4(), name: 'Классическое бикини', cost: 450},
+    { id: uuidv4(), name: 'Глубокое бикини', cost: 650}
   ];
 
+const procedureFullLegs:Row = price.filter(element => element.name === 'Ноги полностью').pop();
+const procedureDeepBikini:Row = price.filter(element => element.name === 'Глубокое бикини').pop();
+const procedureShins:Row = price.filter(element => element.name === 'Голени').pop();
 
-// type Data = {
-//     id: number;
-//     name: string;
-//     cost: number;
-// };
+
+
 
 export default function Price() {
-  const [selectedRows, setSelectedRows] = React.useState([]);
+  const [selectedRows, setSelectedRows] = useState<Set<Row>>(new Set<Row>());
+  const [firstComplexProcedure,setFirstComplexProcedure] = useState(false);
+  const [secondComplexProcedure,setSecondComplexProcedure] = useState(false);
+  const [costOfTheProcedure, changeCostOfTheProcedure] = useState(0);
+
+  const addFirstPriceOfTheProcedure = e => {
+    setFirstComplexProcedure(!firstComplexProcedure);
+    const procedures = [procedureFullLegs,procedureDeepBikini]; 
+    setSelectedRows(new Set([...selectedRows,...procedures]));
+
+    if (!e.target.checked) {
+      selectedRows.delete(procedureFullLegs);
+      selectedRows.delete(procedureDeepBikini);
+      setSelectedRows(new Set([...selectedRows]));
+      return;
+    }
+  }
+
+  const addSecondPriceOfTheProcedure = e => {
+    setSecondComplexProcedure(!secondComplexProcedure);
+    const procedures = [procedureShins,procedureDeepBikini]; 
+    setSelectedRows(new Set([...selectedRows,...procedures]));
+
+    if (!e.target.checked) {
+      selectedRows.delete(procedureShins);
+      selectedRows.delete(procedureDeepBikini);
+      setSelectedRows(new Set([...selectedRows]));
+      return;
+    }
+  }
+
+  useEffect(() => {
+    console.log(selectedRows);
+    let currentCost = 0;
+    for(let row of selectedRows) {
+      currentCost += row.cost;
+    }
+    changeCostOfTheProcedure(currentCost);
+    
+  }, [selectedRows,firstComplexProcedure,secondComplexProcedure]);
 
   const handleRowSelected = React.useCallback(state => {
-    setSelectedRows(state.selectedRows);
-    console.log(state.selectedRows);
-  }, []);
+    if (firstComplexProcedure) {
+      setSelectedRows(new Set([...selectedRows,...state.selectedRows]));  
+    }
+    else if (secondComplexProcedure) {
+      setSelectedRows(new Set([...selectedRows,...state.selectedRows]));  
+    }
+    else 
+      setSelectedRows(new Set([...state.selectedRows]));
+
+  }, [selectedRows]);
 
   return( 
     <div>
@@ -88,13 +134,28 @@ export default function Price() {
           />
       </div>
       <div style={{marginLeft:'0.9rem',marginTop:'1rem'}}>
+        <h4 style={{display:'flex'}}>Комплексы</h4>
         <label style={{display:'flex',flexWrap:'nowrap',alignItems:'baseline'}}>
-          <input type="checkbox" style={{marginRight:'0.5rem',display:'flex'}}/>
+          <input type="checkbox" style={{marginRight:'0.5rem',display:'flex'}} onClick={e => addFirstPriceOfTheProcedure(e)} defaultChecked={false}/>
             <span style={{display:'flex',alignItems:'center'}}>
-              <h4 style={{display:'flex'}}>Комплекс</h4>
-              <h5 style={{display:'flex',fontWeight:'normal',paddingLeft:'20px'}}>Ноги полностью + глубокое бикини + подмышки</h5>
+              <h5 style={{display:'flex',fontWeight:'normal',paddingLeft:'20px'}}>
+                    {procedureFullLegs.name + "+" + procedureDeepBikini.name }{"(подмышечные впадины в подарок) "}
+                    {procedureFullLegs.cost + procedureDeepBikini.cost}{" руб."}
+              </h5>
           </span>
         </label>
+        <label style={{display:'flex',flexWrap:'nowrap',alignItems:'baseline'}}>
+          <input type="checkbox" style={{marginRight:'0.5rem',display:'flex'}} onClick={e => addSecondPriceOfTheProcedure(e)} defaultChecked={false}/>
+            <span style={{display:'flex',alignItems:'center'}}>
+              <h5 style={{display:'flex',fontWeight:'normal',paddingLeft:'20px'}}>
+                    {procedureShins.name + "+" + procedureDeepBikini.name }{"(подмышечные впадины в подарок) "}
+                    {procedureShins.cost + procedureDeepBikini.cost}{" руб."}
+              </h5>
+          </span>
+        </label>
+      </div>
+      <div>
+        <h3>{costOfTheProcedure}{" руб."}</h3>
       </div>
     </div>
     );
